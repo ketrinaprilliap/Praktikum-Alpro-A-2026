36bb30d0efde45eea5fbd944432e4765acbfeb4a
ketrinaprilliap <ketrinaprilia08@gamail.com>
2026-02-16 13:36:02 +0700

    pertemuan 3 Alpro

---
+++ pertemuan3 alpro/Classes_Objects.py
@@ -0,0 +1,21 @@
+class MyClass:
+  x = 5
+
+print(MyClass)
+
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+print(p1.x)
+
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+p2 = MyClass()
+p3 = MyClass()
+
+print(p1.x)
+print(p2.x)
+print(p3.x)
